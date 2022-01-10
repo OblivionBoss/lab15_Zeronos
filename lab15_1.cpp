608f81cca4f13996642af1ba2b1f5a2a30164441
Zeronos <boss16805@gmail.com>
2022-01-10 21:34:01 +0700

    lab15_1 completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    T i, j, c, f; 
+    for (i = 1; i < N; i++)
+    { 
+        c = d[i]; 
+        j = i - 1; 
+        while (j >= 0 && d[j] <= c)
+        { 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = c;
+		cout << "Pass " << i << ":";
+		for(f = 0; f < N; f++){
+			cout << d[f] << " ";
+		}
+		cout << "\n";
+    }
 }
 
 int main(){
